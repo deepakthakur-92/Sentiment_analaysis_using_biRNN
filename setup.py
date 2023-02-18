16301b9c001f007fa676a7fbb884098a6cb6656e
Deepak Thakur <deepak.thakur@3pillarglobal.com>
2023-02-18 22:04:35 +0530

    updated file

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "MLFlow-project-template"
+REPO_NAME = "Sentiment_analaysis_using_biRNN"
 AUTHOR_USER_NAME = "Deepak Thakur"
-SRC_REPO = "src"
+SRC_REPO = "Sentiment_analaysis"
 LIST_OF_REQUIREMENTS = []
 
 
 setup(
     name=SRC_REPO,
-    version="0.0.1",
+    version="1.0.0",
     author=AUTHOR_USER_NAME,
     description="A small package for MLflow app",
     long_description=long_description,
